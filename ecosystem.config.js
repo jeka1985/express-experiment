969bbb8c109546774bb6dd0174c8cd4913fcd74d
Eugene Yemelin <EEmelin@detmir.ru>
2018-07-10 19:29:33 +0300

    Update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,11 +15,11 @@ module.exports = {
       user : 'root',
       ssh_options: "StrictHostKeyChecking=no",
       key: '~/.ssh/digitocean.pub',
-      host : 'localhost',
-      ref  : 'master',
+      host : '178.62.218.228',
+      ref  : 'origin/master',
       repo : 'git@bitbucket.org:eyemelin/my-express-deploy-exp.git',
       path : '/var/www/production',
-      'post-deploy' : 'npm install && npm start --env production'
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
